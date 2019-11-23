bf6ac49eeb42c74f59f3932996262ea86b14efa0
Achimid <achimid@hotmail.com>
2019-11-23 17:30:14 -0300

    initial commit

---
+++ src/main/java/br/com/achimid/noverde/NoverdeApplication.java
@@ -0,0 +1,13 @@
+package br.com.achimid.noverde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoverdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoverdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/achimid/noverde/NoverdeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.achimid.noverde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoverdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
